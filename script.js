d0cf08db4071689f0f603a0067f6dac30d4936ed
fernandobaaena <151637085+fernandobaaena@users.noreply.github.com>
2025-02-06 19:13:03 +0100

    animaciones y correcciones

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const menuIcon = document.getElementById('menu-icon');
 const navLinks = document.getElementById('nav-links');
 
+const elemento = document.querySelector(".welcome-text");
+    const texto = '¡Bienvenido/a a mi Portfolio!';
+
+    function efectoTextTyping(elemento, texto, i = 0) {
+        if (i < texto.length) {
+            elemento.textContent += texto[i];
+            setTimeout(() => efectoTextTyping(elemento, texto, i + 1), 100);
+        }
+    }
+
+    efectoTextTyping(elemento, texto);
+
 menuIcon.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
